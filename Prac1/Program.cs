44b2c4eb9fb46ac73344bb74bf8edc90f96e30ec
Nex1k <Nex1k@MURASAKI>
2026-05-01 20:56:43 +0400

    Добавьте файлы проекта.

---
+++ Prac1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Pract1_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Работа 1";
+            Console.WriteLine("C# --- это современный, с открытым исходным кодом, объектно-ориентированный язык программирования");
+            Console.Write("Нажми любую клавишу...");
+        
+        }
+    }
+}
